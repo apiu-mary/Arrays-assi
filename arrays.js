cd9bcf157f06374e1899166015380f4d0a3a6e6c
apiuuagou@gmail.com <apiuagou@gmail.com>
2023-03-14 16:10:44 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,73 @@
+
+
+//find the last element in array
+let arr1 = [3,7,34,90,12];
+let greatest=arr1[arr1.length -1]
+console.log(greatest);
+
+  arr2 = [true, "green", "where",12,56];
+  let greatest2=arr2[arr2.length-1];
+  console.log(greatest2);
+
+ 
+
+const myPets = ["Cow", "Bird", "Snake", "Dog"];
+myPets.toString();
+console.log(myPets);
+var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+arr3.sort();
+console.log(arr3);
+ var arr = ["apple", "mango", "apple",
+           "orange", "mango", "mango"]; 
+           function remove(arr){return arr.filter((item,index)=> arr.indexOf(item)===index)
+
+         } 
+         console.log(arr) 
+ let Chars=   ["apple", "mango", "apple",
+ "orange", "mango", "mango"];
+ let uniqueChars=[...new Set(Chars)];
+ console.log(uniqueChars);
+ //
+
+ let arr5 = ["the", "way", "x", 4];
+
+  arr6=["the", "way", "x", 4];
+  let z="we";
+  if (z=arr6){
+    console.log();
+
+  }
+  else{
+    console.log("the search is not found")
+  }
+ //sort the following string
+let word = "sevink";
+let word1=word.split("").sort().join("");
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+ 
